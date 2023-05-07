bb4a0ecd827f563548b0c64897097363ba55d7cb
Quiny Hsieh <quinyhsieh@quinys-mbp.lan>
2023-05-07 21:36:05 +0800

    first commit

---
+++ scripts/index.js
@@ -0,0 +1,179 @@
+let app = new Vue({
+    el: '#app',
+    data: {
+        items: ["Have no idea what to eat?"],
+        index: 0,
+        running: false,
+        eventBuffer: null,
+        speed: 5,
+        startTime: 160,
+        durning: 100,
+        fastDurning: 1500
+    },
+    methods: {
+        getRandom: function (min, max) {
+            this.index = Math.floor(Math.random() * max) + min
+        },
+        delay: function (delayInms) {
+            return new Promise(resolve => {
+                setTimeout( () => {
+                    resolve(2)
+                }, delayInms)
+            })
+        },
+        runCycle: async function () {
+            $('[data-toggle="tooltip"]').tooltip('dispose')
+
+            let tmpTime = this.startTime
+            this.running = true
+            this.items = items.groups[items.activedIndex].items.split('\n')
+            if (this.items.length === 1) {
+                if (this.items[0] === "") {
+                    this.items = ["-"]
+                }
+                this.index = 0
+                this.running = false
+                return
+            }
+
+            this.eventBuffer = setInterval( () => {
+                this.getRandom(0, this.items.length)
+            }, this.startTime)
+
+            while(tmpTime > 0){
+                tmpTime -= this.speed
+                setTimeout( () => {
+                    clearInterval(this.eventBuffer)
+                    this.eventBuffer = setInterval( () => {
+                        this.getRandom(0, this.items.length)
+                    }, tmpTime)
+                }, this.durning)
+                let delayres = await this.delay(this.durning)
+            }
+            
+            let delayres = await this.delay(this.fastDurning)
+
+            while(tmpTime <= this.startTime){
+                tmpTime += this.speed
+                setTimeout( () => {
+                    clearInterval(this.eventBuffer)
+                    this.eventBuffer = setInterval( () => {
+                        this.getRandom(0, this.items.length)
+                    }, tmpTime)
+                }, this.durning)
+                let delayres = await this.delay(this.durning)
+            }
+            
+            setTimeout( () => {
+                clearInterval(this.eventBuffer)
+                this.running = false
+                $('[data-toggle="tooltip"]').tooltip()
+            }, this.durning)
+        }
+    }
+})
+
+let items = new Vue({
+    el: '#items',
+    data: {
+        groups: [
+            {
+                "title": "Default Group",
+                "items": "Enter options here and separate different options with newlines.\nFor example:\nOption 1\nOption 2\nClick the plus sign next to add a group and select the group to use by clicking on the circle."
+            }
+        ],
+        activedIndex: 0,
+        actived: "group-0"
+    },
+    methods: {
+        emptyGroup: function () {
+            this.actived = "group-0"
+            this.groups = [
+                {
+                "title": "Default Group",
+                "items": "Enter options here and separate different options with newlines.\nFor example:\nOption 1\nOption 2\nClick the plus sign next to add a group and select the group to use by clicking on the circle."
+                }
+            ]
+        },
+        addGroup: function () {
+            this.groups.push({"title": "", "items": ""})
+        },
+        setActive: function (index) {
+            this.actived = "group-" + index
+            this.activedIndex = index
+        },
+        importOption: function (e) {
+            var fr=new FileReader()
+            var text='';
+            fr.addEventListener('load', (event) => {
+                text = event.target.result
+                this.groups = JSON.parse(text)
+            });
+            fr.readAsText(e.target.files[0])
+        },
+        exportOption: function () {
+            var json = new Blob([JSON.stringify(this.groups, null, '\t')], {type: 'text/plain'})
+            var jfile = window.URL.createObjectURL(json)
+            const a = document.createElement('a')
+            a.style.display = 'none'
+            a.href = jfile
+            a.download = `random-picker-setting.json`
+            document.body.appendChild(a)
+            a.click()
+            window.URL.revokeObjectURL(jfile)
+        }
+    },
+    beforeMount: function() { 
+        if (Cookies.get('items')) {
+            this.groups = JSON.parse(Cookies.get('items'))
+        }
+     },
+    updated: function() {
+        Cookies.set('items', JSON.stringify(this.groups))
+    }
+})
+
+let depLib = new Vue({
+    el: '#dependence-lib',
+    data: {
+        dependence: website_info.dependence
+    },
+    mounted: function () {
+        $('[data-toggle="tooltip"]').tooltip()
+    }
+})
+
+let ver = new Vue({
+    el: '#version',
+    data: {
+        versions: website_info.changeLog
+    }
+})
+
+let copyright = new Vue({
+    el: '#copyright',
+    data: {
+        versions: website_info.changeLog.reverse()[0].ver
+    }
+})
+
+let b2t = new Vue({
+    el: '#b2t-container',
+    data: {
+        isTop: true,
+        styles: {
+            bottom: '-120px'
+        }
+    },
+    mounted: function () {
+        window.addEventListener('scroll', () => {
+            this.isTop = !(document.body.scrollTop > 0);
+            this.styles.bottom = this.isTop ? '-120px' : '0' 
+        })
+    }
+})
+
+let blogo = new Vue({
+    el: '#blogo',
+    data: app._data
+})
---
+++ scripts/website-info.js
@@ -0,0 +1,34 @@
+const website_info = {
+    "dependence": [
+        {
+            "name": "Bootstrap 4",
+            "description": "前端框架",
+            "url": "//getbootstrap.com/",
+            "logo": "bootstrap.svg"
+        },
+        {
+            "name": "Vue.js",
+            "description": "前端渲染引擎",
+            "url": "//vuejs.org",
+            "logo": "vuejs.png"
+        },
+        {
+            "name": "CSS Shake",
+            "description": "震動效果動畫",
+            "url": "//elrumordelaluz.github.io/csshake/",
+            "logo": "csshake.png"
+        },
+        {
+            "name": "FontAwesome 5",
+            "description": "網頁圖示字體庫",
+            "url": "//fontawesome.com/",
+            "logo": "fontawesome.png"
+        },
+        {
+            "name": "Google Fonts",
+            "description": "網頁字體提供",
+            "url": "//fonts.google.com/",
+            "logo": "googlefont.png"
+        }
+    ]
+}
